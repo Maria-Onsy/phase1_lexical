1b055e51c59c47c339ac96e80a03f9f93225ce68
Nada Fathy Ali <48325643+nada-fathy@users.noreply.github.com>
2021-05-06 22:50:43 +0200

    Add files via upload

---
+++ include/DFA.h
@@ -0,0 +1,23 @@
+#ifndef DFA_H
+#define DFA_H
+
+//#include "Exp_Node.h"
+#include "Final_NDFA.h"
+#include "DFA_state.h"
+#include "NDFA_state.h"
+//#include "NDFA.h"
+
+#include <list>
+using namespace std;
+
+class DFA
+{
+public:
+    int num;
+    list<DFA_state> allStates;
+    int start;
+    construct_DFA(Final_NDFA nd);
+    list<NDFA_state*> getEps(Final_NDFA nd ,int state);
+};
+
+#endif // DFA_H
---
+++ include/DFA_state.h
@@ -0,0 +1,26 @@
+#ifndef DFA_STATE_H
+#define DFA_STATE_H
+
+#include <list>
+#include <string>
+#include "NDFA_state.h"
+using namespace std;
+
+
+class DFA_state
+{
+public:
+    class line {
+    public:
+        list<int> to;
+        char input;
+    };
+
+    list<NDFA_state*> ids;
+    bool stable;
+    list<line> trans;
+};
+
+
+
+#endif // DFA_STATE_H
