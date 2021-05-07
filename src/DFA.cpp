03bf4394602196cbd0662f147aca2bc60b39e744
Nada Fathy Ali <48325643+nada-fathy@users.noreply.github.com>
2021-05-07 18:20:23 +0200

    Update DFA.cpp

--- src/DFA.cpp
+++ src/DFA.cpp
@@ -15,9 +15,14 @@ DFA::construct_DFA(Final_NDFA nd) {
 	list<NDFA_state> startl = getEps(nd ,start);
 	startl.unique();   // remove dublicate states
 	DFA_state s;
+    list<int> startints;///
+    list<NDFA_state> ::iterator getints;///
+    for(getints=startl.begin();getints!=startl.end();++getints){///
+        startints.push_back((*getints).id);///
+    }///
 
 	//assign the list of start states to ids list of start deterministic state
-    s.ids .insert(s.ids.end(), startl.begin(), startl.end());
+    s.ids .insert(s.ids.end(), startints.begin(), startints.end());
     allStates.push_back(s);
 	list<NDFA_state> ::iterator it;
 
@@ -29,17 +34,26 @@ while(!q.empty()){
     q.pop();
 
    // check if the deter. state is stable or not
-    list<NDFA_state> :: iterator isstable;
+    list<int> :: iterator isstable;    ////
     for (isstable = s.ids.begin(); isstable != s.ids.end(); ++isstable) {
-        if((*isstable).stable == true){
+       // if((*isstable).stable == true){  ////
+           // s.stable=true;
+       // }
+       if(nd.get_state(*isstable)->stable==true){
             s.stable=true;
-        }
+       }
     }
 
 
 	list<char> validinputs;   //inputs from the current deter. state
     list<NDFA_state> ll;      // state of non deter. states of the current deter. state(s)
-    ll.insert(ll.end(), s.ids.begin(), s.ids.end());
+
+    list<NDFA_state> startints2;///
+    list<int> ::iterator getints;///
+    for(getints=s.ids.begin();getints!=s.ids.end();++getints){///
+        startints2.push_back(*nd.get_state((*getints)));///
+    }///
+    ll.insert(ll.end(), startints2.begin(), startints2.end());
 
 	//extract all possible inputs from the start state s
 	for (it = ll.begin(); it != ll.end(); ++it) {
@@ -56,46 +70,49 @@ while(!q.empty()){
 	validinputs.unique();
 
 
-		list<NDFA_state> ::iterator nextit;
-		list <NDFA_state> currentids = s.ids;
-		for (nextit = currentids.begin(); nextit != currentids.end(); ++nextit) {  // iterate over NDFS nodes of the current DFA node
-			list<char> ::iterator inputit;
+            list<char> ::iterator inputit;
 			for (inputit = validinputs.begin(); inputit != validinputs.end(); ++inputit) {  // loop over valid inputs
-				list<NDFA_state> next;
-				list<NDFA_state::line> ::iterator lineit;
-				list< NDFA_state::line> linel = (*nextit).trans;
-				for (lineit = linel.begin(); lineit != linel.end(); ++lineit) {
-					if (lineit->input == *inputit) {
-						NDFA_state tmp = *nd.get_state(lineit->to);
-						next.push_back(tmp);
-						list<NDFA_state> ep = getEps(nd, lineit->to);
-						list<NDFA_state> ::iterator epit;
-						for (epit = ep.begin(); epit != ep.end(); ++epit) {  // loop over eps transitions from the destination state and add them to next
-							next.push_back(*epit);
-					}
-				}
-			}
-				if (!next.empty()) {
-					next.unique();
-					DFA_state::line *newl;
-					DFA_state news;   // next state after entering inputit to the start state
+                list<int> ::iterator nextit;     ///
+                list <int> currentids = s.ids; ///
+                list<int> next;///
+                for (nextit = currentids.begin(); nextit != currentids.end(); ++nextit) {  // iterate over NDFS nodes of the current DFA node
 
-					news.ids .insert(news.ids .end(), next.begin(), next.end());
-
-					newl->input = *inputit;
-					newl->to = &news;
-					s.trans.push_back(*newl);
-                    q.push(news);
-                    counter++;
-                    allStates.push_back(news);
-				}
-
+                    list<NDFA_state::line> ::iterator lineit;
+                    list< NDFA_state::line> linel = nd.get_state(*nextit)->trans; ///
+                    for (lineit = linel.begin(); lineit != linel.end(); ++lineit) {
+                        if (lineit->input == *inputit) {
+                            NDFA_state tmp = *nd.get_state(lineit->to);
+                            next.push_back(tmp.id);///
+                            list<NDFA_state> ep = getEps(nd, lineit->to);
+                            list<NDFA_state> ::iterator epit;
+                            for (epit = ep.begin(); epit != ep.end(); ++epit) {  // loop over eps transitions from the destination state and add them to next
+                                next.push_back((*epit).id);  ///
+                        }
+                    }
+                }
             }
 
+                    if (!next.empty()) {
+                        next.unique();
+                        DFA_state::line *newl;
+                        DFA_state news;   // next state after entering inputit to the start state
+
+                        news.ids .insert(news.ids .end(), next.begin(), next.end());
+
+                        newl->input = *inputit;
+                        newl->to = &news;
+                        s.trans.push_back(*newl);
+                        q.push(news);
+                        counter++;
+                        allStates.push_back(news);
+                    }
+
+                }
+
+
+
+
         }
-
-
-    }
 /////
 
 }
